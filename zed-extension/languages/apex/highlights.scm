50f40ebd2abe20e6d11f828b7c41cfa84ba2fa63
Cesar Parra <cesar.parra@gmail.com>
2026-01-05 13:03:40 +0100

    Improve Apex highlights and variable coverage for Zed

--- zed-extension/languages/apex/highlights.scm
+++ zed-extension/languages/apex/highlights.scm
@@ -1,22 +1,27 @@
-; Apex (Salesforce) highlights for Zed
-;
-; Goals:
-; - Only reference node / token types that exist in the Apex Tree-sitter grammar.
-; - Avoid "impossible pattern" / "invalid node type" errors that prevent the language from loading.
-; - Keep this conservative and extensible.
+;; Apex (Salesforce) highlights for Zed
+;;
+;; Goals:
+;; - Improve coverage (esp. variables) using the grammar’s reference highlight ideas.
+;; - Keep captures Zed-theme compatible (avoid non-standard semantic tokens).
+;; - Avoid query errors: only use node/token types that exist in the Apex grammar.
+;;
+;; Notes:
+;; - Zed supports captures like @keyword, @type, @function, @variable, @property, etc.
+;; - This file intentionally uses conservative, structural patterns for identifiers,
+;;   so variable declarations and common usages light up reliably.
 
-; ------------------------------------------------------------------------------
+;------------------------------------------------------------------------------
 ; Comments
-; ------------------------------------------------------------------------------
+;------------------------------------------------------------------------------
 
 [
   (line_comment)
   (block_comment)
 ] @comment
 
-; ------------------------------------------------------------------------------
+;------------------------------------------------------------------------------
 ; Literals
-; ------------------------------------------------------------------------------
+;------------------------------------------------------------------------------
 
 (string_literal) @string
 
@@ -26,107 +31,35 @@
 ] @number
 
 (boolean) @boolean
-
 (null_literal) @constant
 
-; ------------------------------------------------------------------------------
-; Declarations / identifiers
-; ------------------------------------------------------------------------------
+;------------------------------------------------------------------------------
+; Punctuation / delimiters / brackets
+;------------------------------------------------------------------------------
 
-; Types
-(class_declaration
-  name: (identifier) @type)
-
-(interface_declaration
-  name: (identifier) @type)
-
-(enum_declaration
-  name: (identifier) @type)
-
-; Enum members
-(enum_constant
-  name: (identifier) @constant)
-
-; Methods: declarations + calls
-(method_declaration
-  name: (identifier) @function)
-
-(method_invocation
-  name: (identifier) @function)
-
-; Constructors
-(constructor_declaration
-  name: (identifier) @type)
-
-; Parameters and local variables
-(formal_parameter
-  name: (identifier) @variable)
-
-(local_variable_declaration
-  (variable_declarator
-    name: (identifier) @variable))
-
-; Fields / properties
-(field_declaration
-  declarator: (variable_declarator
-    name: (identifier) @property))
-
-(field_access
-  field: (identifier) @property)
-
-; ------------------------------------------------------------------------------
-; Keywords
-; ------------------------------------------------------------------------------
-; NOTE: Prefer grammar token types over guessed strings.
-; In this grammar, many keywords exist as unnamed token types with lowercase names
-; (e.g. "class", "public", "return"). Use those token strings directly.
+"@" @punctuation.special
 
 [
-  "class"
-  "interface"
-  "enum"
-  "extends"
-  "implements"
-  "new"
-  "return"
-  "if"
-  "else"
-  "switch"
-  "when"
-  "for"
-  "while"
-  "do"
-  "break"
-  "continue"
-  "try"
-  "catch"
-  "finally"
-  "throw"
-  "trigger"
-  "on"
+  "."
+  ","
+  ";"
+  ":"
+] @punctuation.delimiter
 
-  "public"
-  "private"
-  "protected"
-  "global"
-  "static"
-  "final"
-  "virtual"
-  "abstract"
-  "override"
-  "transient"
-  "webservice"
-  "testMethod"
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+  "<"
+  ">"
+] @punctuation.bracket
 
-  ; Sharing modifiers are distinct token types in this grammar.
-  (with_sharing)
-  (without_sharing)
-  (inherited_sharing)
-] @keyword
-
-; ------------------------------------------------------------------------------
-; Operators & punctuation
-; ------------------------------------------------------------------------------
+;------------------------------------------------------------------------------
+; Operators
+;------------------------------------------------------------------------------
 
 (assignment_operator) @operator
 (update_operator) @operator
@@ -170,32 +103,213 @@
   "=>"
 ] @operator
 
-"@" @punctuation.special
+;------------------------------------------------------------------------------
+; Keywords / modifiers
+;------------------------------------------------------------------------------
 
 [
-  "."
-  ","
-  ";"
-  ":"
-] @punctuation.delimiter
+  "break"
+  "catch"
+  "class"
+  "continue"
+  "do"
+  "else"
+  "enum"
+  "extends"
+  "finally"
+  "for"
+  "get"
+  "if"
+  "implements"
+  "instanceof"
+  "interface"
+  "new"
+  "on"
+  "return"
+  "set"
+  "switch"
+  "throw"
+  "try"
+  "trigger"
+  "when"
+  "while"
 
+  "public"
+  "private"
+  "protected"
+  "global"
+  "static"
+  "final"
+  "virtual"
+  "abstract"
+  "override"
+  "transient"
+  "webservice"
+  "testMethod"
+] @keyword
+
+; Sharing modifiers are distinct token types in this grammar.
 [
-  "("
-  ")"
-  "{"
-  "}"
-  "["
-  "]"
-  "<"
-  ">"
-] @punctuation.bracket
+  (with_sharing)
+  (without_sharing)
+  (inherited_sharing)
+] @keyword
 
-; ------------------------------------------------------------------------------
+;------------------------------------------------------------------------------
+; Types
+;------------------------------------------------------------------------------
+
+; Named types declared in-file
+(class_declaration
+  name: (identifier) @type)
+
+(interface_declaration
+  name: (identifier) @type)
+
+(enum_declaration
+  name: (identifier) @type)
+
+; Superclass / implements references
+(class_declaration
+  (superclass) @type)
+
+(interfaces
+  (type_list
+    (type_identifier) @type))
+
+; Type identifiers in common type positions
+(type_identifier) @type
+(scoped_type_identifier (type_identifier) @type)
+
+(generic_type
+  (type_identifier) @type)
+
+(type_arguments
+  (type_identifier) @type)
+
+(array_type
+  element: (type_identifier) @type)
+
+(object_creation_expression
+  type: (type_identifier) @type)
+
+(array_creation_expression
+  type: (type_identifier) @type)
+
+;------------------------------------------------------------------------------
+; Enums
+;------------------------------------------------------------------------------
+
+(enum_constant
+  name: (identifier) @constant)
+
+;------------------------------------------------------------------------------
+; Functions / methods
+;------------------------------------------------------------------------------
+
+(method_declaration
+  name: (identifier) @function)
+
+(method_invocation
+  name: (identifier) @function)
+
+; Common builtins
+(super) @function
+
+;------------------------------------------------------------------------------
+; Variables (this is the main area to improve)
+;------------------------------------------------------------------------------
+
+; Parameters
+(formal_parameter
+  name: (identifier) @variable)
+
+(method_declaration
+  (formal_parameters
+    (formal_parameter
+      name: (identifier) @variable)))
+
+; Local variable declarations
+(local_variable_declaration
+  declarator: (variable_declarator
+    name: (identifier) @variable))
+
+; Field declarations
+(field_declaration
+  declarator: (variable_declarator
+    name: (identifier) @property))
+
+; Enhanced for: `for (Type name : value)`
+(enhanced_for_statement
+  name: (identifier) @variable)
+
+(enhanced_for_statement
+  value: (identifier) @variable)
+
+; LHS assignments (helps highlight variables when declared elsewhere)
+(assignment_expression
+  left: (identifier) @variable)
+
+; Identifier occurrences in common expression contexts
+(return_statement
+  (identifier) @variable)
+
+(bound_apex_expression
+  (identifier) @variable)
+
+(instanceof_expression
+  left: (identifier) @variable
+  right: (type_identifier) @type)
+
+(cast_expression
+  type: (type_identifier) @type
+  value: (identifier) @variable)
+
+(binary_expression
+  (identifier) @variable)
+
+(switch_expression
+  condition: (identifier) @variable)
+
+;------------------------------------------------------------------------------
+; Properties / member access
+;------------------------------------------------------------------------------
+
+(field_access
+  field: (identifier) @property)
+
+; Best-effort: If something looks like a TypeName.member, treat the prefix as a type.
+; This is useful for `System.debug`, `Account.SObjectType`, etc.
+((scoped_identifier
+  scope: (identifier) @type)
+ (#match? @type "^[A-Z]"))
+
+((method_invocation
+  object: (identifier) @type)
+ (#match? @type "^[A-Z]"))
+
+;------------------------------------------------------------------------------
 ; Annotations
-; ------------------------------------------------------------------------------
+;------------------------------------------------------------------------------
 
 (annotation
   name: (identifier) @attribute)
 
 (annotation_key_value
   (identifier) @property)
+
+;------------------------------------------------------------------------------
+; SOQL-ish query constructs (from grammar nodes)
+;------------------------------------------------------------------------------
+
+(dml_type) @function
+
+(trigger_declaration
+  name: (identifier) @type
+  object: (identifier) @type
+  (trigger_event) @keyword
+  ("," (trigger_event) @keyword)*)
+
+(when_sobject_type
+  (type_identifier) @type
+  (identifier) @variable)
